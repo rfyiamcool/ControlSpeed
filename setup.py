d1416b4bf0954d985968ad7a0dd7f30781d023c8
芮峰云 <rfyiamcool@163.com>
2015-11-14 23:26:32 +0800

    commit pypi ,version 1.0

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+        name = "controlspeed",
+        version = "1.0",
+        author = "ruifengyun",
+        author_email = "rfyiamcool@163.com",
+        description = "Control function call speed ,support network mode by redis",
+        license = "MIT",
+        keywords = ["controlspeed","fengyun"],
+        url = "https://github.com/rfyiamcool",
+        packages = find_packages(),
+        long_description = read('README.md'),
+        install_requires=['redis'],
+        classifiers = [
+             'Development Status :: 2 - Pre-Alpha',
+             'Intended Audience :: Developers',
+             'License :: OSI Approved :: MIT License',
+             'Programming Language :: Python :: 2.7',
+             'Programming Language :: Python :: 3.0',
+             'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+)
+
